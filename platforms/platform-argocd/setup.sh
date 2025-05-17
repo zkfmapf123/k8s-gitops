3074548cc035308fa14552205ca5635fe214925d
leedonggyu <zkfmapf123@naver.com>
2025-05-17 15:36:27 +0900

    wip-15584564

--- platforms/platform-argocd/setup.sh
+++ platforms/platform-argocd/setup.sh
@@ -2,4 +2,4 @@
 kubectl apply -f ./install.yaml -n platform
 
 # ArgoCD Web UI 접속시 admin 패스워드 확인 방법
-kubectl -n platform get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+kubectl -n  get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
